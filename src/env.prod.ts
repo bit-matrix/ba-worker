2b88ea8d8ed66cf812dbfd4f98b293997dc8fe73
Penguen Umut <uuisik@gmail.com>
2021-12-16 21:52:53 +0300

    env

---
+++ src/env.prod.ts
@@ -0,0 +1,3 @@
+// prod
+export const ELECTRS_URL = process.env.DB_URL || "http://127.0.0.1:3000/";
+export const DB_URL = process.env.DB_URL || "http://127.0.0.1:8899/";
---
+++ src/env.ts
@@ -0,0 +1,3 @@
+// local
+export const ELECTRS_URL = process.env.DB_URL || "https://electrs.bitmatrix-aggregate.com/";
+export const DB_URL = process.env.DB_URL || "https://db.bitmatrix-aggregate.com/";
