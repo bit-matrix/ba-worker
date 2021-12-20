0ad5a35ebb28cd2f91b3fcbe4ed88b5ffe8e4361
Penguen Umut <uuisik@gmail.com>
2021-12-21 01:54:17 +0300

    telegram for testing

---
+++ src/helper/sendTelegramMessage.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BITMATRIX_WATCHTOWER = {
+  token: "AAGLP1BTJQ9i7Y2Old7MsV3daCWg0RVLqzs",
+  chatId: "-707696434",
+};
+
+export const sendTelegramMessage = (message: string) => {
+  console.log("sendTelegramMessage", message + "\n ------------- ");
+
+  try {
+    const url = "https://api.telegram.org/bot2145331675:" + BITMATRIX_WATCHTOWER.token + "/sendMessage?chat_id=" + BITMATRIX_WATCHTOWER.chatId + "&parse_mode=html";
+    return axios.get(
+      // "https://api.telegram.org/bot2145331675:AAGLP1BTJQ9i7Y2Old7MsV3daCWg0RVLqzs/sendMessage?chat_id=-707696434&parse_mode=html&text=" + message + "\n ------------- "
+      url + "&text=" + message + "\n ------------- "
+    );
+  } catch (error) {
+    console.log("sendTelegramMessage.error", error);
+  }
+};
--- src/workers/findNewCtxWorker/index.ts
+++ src/workers/findNewCtxWorker/index.ts
@@ -1,6 +1,7 @@
 import { TxDetail } from "@bitmatrix/esplora-api-client";
 import { BmBlockInfo, BmCtxNew, CallData, Pool } from "@bitmatrix/models";
 import { ctxNewSave } from "../../business/db-client";
+import { sendTelegramMessage } from "../../helper/sendTelegramMessage";
 import { isCtxWorker } from "./isCtxWorker";
 
 export const findNewCtxWorker = async (pool: Pool, newBmBlockInfo: BmBlockInfo, newTxDetails: TxDetail[]) => {
@@ -19,6 +20,7 @@ export const findNewCtxWorker = async (pool: Pool, newBmBlockInfo: BmBlockInfo, 
 
         const bmCtxNew: BmCtxNew = { callData, commitmentTx: { txid: newTxDetail.txid, ...newBmBlockInfo } };
         await ctxNewSave(pool.id, bmCtxNew);
+        sendTelegramMessage("new ctx: https://db.bitmatrix-aggregate.com/ctx/43a2f4ef8ce286e57ab3e39e6da3741382ba542854a1b28231a7a5b8ba337fcd/" + bmCtxNew.commitmentTx.txid);
       }
     }
   } catch (error) {
