ea866cbe086246f311f6739ab4366ccb537cb5a2
Penguen Umut <uuisik@gmail.com>
2021-12-20 20:20:21 +0300

    createPoolTx

---
+++ src/workers/createPoolTxWorker/index.ts
@@ -0,0 +1,13 @@
+import { BmBlockInfo } from "../../business/data/models/BmInfo";
+import { BmCtxNew } from "../../business/data/models/BmTx";
+import { Pool } from "../../business/data/models/Pool";
+
+export const createPoolTxWorker = async (pool: Pool, newBmBlockInfo: BmBlockInfo, newCtxs: BmCtxNew[]) => {
+  console.log("Create ptx worker for newCtxs started for pool: " + pool.id + ". newBlockheight: " + newBmBlockInfo.block_height + ", newCtxs.count: " + newCtxs.length);
+  if (newCtxs.length !== 0) {
+    const sortedNewCtxs = newCtxs.sort((a, b) => b.callData.orderingFee - a.callData.orderingFee);
+    const bestCtx = sortedNewCtxs[0];
+    console.log("bestCtx for pool tx: ", bestCtx);
+    // TODO
+  }
+};
