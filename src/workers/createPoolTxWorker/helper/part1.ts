efb834745c30e0090d584bea9e84b8465b16668d
Penguen Umut <uuisik@gmail.com>
2021-12-22 01:09:23 +0300

    part1

---
+++ src/workers/createPoolTxWorker/helper/part1.ts
@@ -0,0 +1,37 @@
+import { hexLE } from "@script-wiz/wiz-data";
+
+// tx inputs
+export const part1 = (unspentTxid: string, commitmentTxid: string): string => {
+  const latestPoolTxidLE = hexLE(unspentTxid);
+  const ctxidLE = hexLE(commitmentTxid);
+
+  return (
+    "02000000" +
+    "01" +
+    "06" +
+    latestPoolTxidLE +
+    "00000000" +
+    "00" +
+    "01000000" +
+    latestPoolTxidLE +
+    "01000000" +
+    "00" +
+    "01000000" +
+    latestPoolTxidLE +
+    "02000000" +
+    "00" +
+    "01000000" +
+    latestPoolTxidLE +
+    "03000000" +
+    "00" +
+    "01000000" +
+    ctxidLE +
+    "01000000" +
+    "00" +
+    "01000000" +
+    ctxidLE +
+    "02000000" +
+    "00" +
+    "01000000"
+  );
+};
