e3646f46688ff0aff9bbab10a8a975044e330d67
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-23 19:41:54 +0300

    ctxWorker:if is newCtxWorker sendTelegramMessage

--- src/workers/findNewCtxWorker/ctxWorker.ts
+++ src/workers/findNewCtxWorker/ctxWorker.ts
@@ -1,27 +1,25 @@
 import { Block, TxDetail, TxOutSpend, esploraClient } from "@bitmatrix/esplora-api-client";
-import { BmConfig, BmCtxNew, BmPtx, BmTxInfo, CallData, CallDataBase, CallDataValue, CommitmentOutput, Pool } from "@bitmatrix/models";
-import { ptxSave } from "../../business/db-client";
+import { BmConfig, BmCtxNew, BmPtx, BmTxInfo, CallData, CallDataBase, CallDataValue, CALL_METHOD, CommitmentOutput, Pool } from "@bitmatrix/models";
+import { ctxNewSave, ptxSave } from "../../business/db-client";
+import { sendTelegramMessage } from "../../helper/sendTelegramMessage";
 import { checkTweakedPubkey } from "./steps/checkTweakedPubkey";
 import { getCallDataBase } from "./steps/getCallDataBase";
 import { getCallDataValue } from "./steps/getCallDataValue";
 
-const isCtxSpend = async (ctx: BmCtxNew): Promise<boolean> => {
-  const txOutSpends = await esploraClient.txOutspends(ctx.commitmentTx.txid);
-  const outSpend = txOutSpends[1];
+const isCtxSpend = async (ctx: BmCtxNew): Promise<BmPtx | undefined> => {
+  const txOutSpends: TxOutSpend[] = await esploraClient.txOutspends(ctx.commitmentTx.txid);
+  const outSpend: TxOutSpend = txOutSpends[1];
   if (outSpend.spent && outSpend.status) {
     const poolTx: BmTxInfo = {
       txid: ctx.commitmentTx.txid,
       block_hash: outSpend.status.block_hash,
       block_height: outSpend.status?.block_height,
     };
-    const value: BmPtx = { ...ctx, poolTx };
+    const bmPoolTx: BmPtx = { ...ctx, poolTx };
 
-    ptxSave(ctx.commitmentTx.txid, value);
-
-    return false;
-  } else {
-    return true;
+    return bmPoolTx;
   }
+  return;
 };
 
 export const isCtxWorker = async (pool: Pool, poolConfig: BmConfig, newTxDetail: TxDetail, newBlock: Block): Promise<BmCtxNew | undefined> => {
@@ -60,14 +58,29 @@ export const isCtxWorker = async (pool: Pool, poolConfig: BmConfig, newTxDetail:
   return;
 };
 
-export const isNewCtxWorker = async (pool: Pool, poolConfig: BmConfig, newTxDetail: TxDetail, newBlock: Block): Promise<BmCtxNew | undefined> => {
+export const isNewCtxWorker = async (pool: Pool, poolConfig: BmConfig, newTxDetail: TxDetail, newBlock: Block): Promise<BmCtxNew | BmPtx | undefined> => {
   const ctx = await isCtxWorker(pool, poolConfig, newTxDetail, newBlock);
   if (ctx) {
-    const isSpendCtx = await isCtxSpend(ctx);
-    if (isSpendCtx) {
-      return;
-    } else {
+    const bmPtx: BmPtx | undefined = await isCtxSpend(ctx);
+    if (!bmPtx) {
+      await ctxNewSave(pool.id, ctx);
+
+      const val =
+        ctx.callData.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN ? ctx.callData.value.quote : CALL_METHOD.SWAP_TOKEN_FOR_QUOTE ? ctx.callData.value.token : ctx.callData.value.lp;
+      sendTelegramMessage(
+        "Commmitment Tx: <code>" +
+          ctx.commitmentTx.txid +
+          "</code>\n" +
+          "Commmitment Data: <b>Method</b>: <code>" +
+          ctx.callData.method +
+          "</code>, <b>Value</b>: <code>" +
+          val +
+          "</code>"
+      );
       return ctx;
+    } else {
+      ptxSave(ctx.commitmentTx.txid, bmPtx);
+      return bmPtx;
     }
   }
   return;
