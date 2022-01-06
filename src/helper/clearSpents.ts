b7d9abc8b7deddf8a56a3d214658c5c0d207cacf
huseyinsaritas <hsynsrtss@gmail.com>
2022-01-07 01:49:17 +0300

    clearCtxSpents func bug fix

--- src/helper/clearSpents.ts
+++ src/helper/clearSpents.ts
@@ -1,17 +1,17 @@
 import { esploraClient, TxOutSpend } from "@bitmatrix/esplora-api-client";
-import { ctxSpentClear } from "../business/db-client";
+import { ctxNewDelete } from "../business/db-client";
 import { BmCtxNew, Pool } from "@bitmatrix/models";
 
 export const clearCtxSpents = async (pool: Pool, ctxs: BmCtxNew[]): Promise<BmCtxNew[]> => {
-  const newCtxs = [...ctxs];
+  let newCtxs = [...ctxs];
 
-  for (let i = 0; i < newCtxs.length; i++) {
-    const ctx = newCtxs[i];
+  for (let i = 0; i < ctxs.length; i++) {
+    const ctx = ctxs[i];
     const txOutSpends: TxOutSpend[] = await esploraClient.txOutspends(ctx.commitmentTx.txid);
     const outSpend: TxOutSpend = txOutSpends[1];
 
     if (outSpend.spent) {
-      await ctxSpentClear(pool.id, ctx.commitmentTx.txid);
+      await ctxNewDelete(pool.id, ctx.commitmentTx.txid);
       newCtxs.splice(i, 1);
     }
   }
