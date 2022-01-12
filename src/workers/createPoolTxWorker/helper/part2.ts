d080d63e51bca8679cc6a450fafa89faab6ff9bb
Penguen Umut <uuisik@gmail.com>
2022-01-13 01:05:43 +0300

    case 1,2 values, outputs, new controllers

--- src/workers/createPoolTxWorker/helper/part2.ts
+++ src/workers/createPoolTxWorker/helper/part2.ts
@@ -8,32 +8,34 @@ export const part2 = (pool: Pool, poolConfig: BmConfig, callData: CallData): str
   const tokenAssetLE = hexLE(pool.token.asset);
   const lpAssetLE = hexLE(pool.lp.asset);
   const qouteAssetLE = hexLE(pool.quote.asset);
+  const ctxInputValue = callData.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN ? callData.value.quote : callData.value.token;
 
   let recipientAssetLE = callData.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN ? hexLE(pool.token.asset) : hexLE(pool.quote.asset);
-
-  const ctxInputValue = callData.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN ? callData.value.quote : callData.value.token;
-  const recipientValueNumber = calcRecipientValue(pool, ctxInputValue, callData.method, poolConfig.recipientValueMinus);
-  let recipientValue = toHex64BE(recipientValueNumber);
-  const recipientScriptPubkey = getRecipientScriptPubkey(callData.recipientPublicKey);
-
-  const { txFee, serviceCommission } = getTxFeeServiceCommission(poolConfig.baseFee.number, poolConfig.serviceFee.number, callData.orderingFee); //  438, 1452   ???
-
+  let recipientValueNumber = calcRecipientValue(pool, ctxInputValue, callData.method, poolConfig.recipientValueMinus);
   let poolNewQuoteValue =
     callData.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN ? toHex64BE(Number(pool.quote.value) + callData.value.quote) : toHex64BE(Number(pool.quote.value) - recipientValueNumber);
   let poolNewTokenValue =
     callData.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN ? toHex64BE(Number(pool.token.value) - recipientValueNumber) : toHex64BE(Number(pool.token.value) + callData.value.token);
-  let poolNewLPValue = toHex64BE(Number(pool.lp.value));
 
   const isOutOfSlippage: boolean = recipientValueNumber < Number(callData.slippageTolerance);
   if (isOutOfSlippage) {
+    recipientAssetLE = callData.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN ? hexLE(pool.quote.asset) : hexLE(pool.token.asset);
+    recipientValueNumber = callData.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN ? callData.value.quote : callData.value.token;
     poolNewQuoteValue = toHex64BE(Number(pool.quote.value));
     poolNewTokenValue = toHex64BE(Number(pool.token.value));
-
-    recipientAssetLE = callData.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN ? hexLE(pool.quote.asset) : hexLE(pool.token.asset);
-    const recipientValueNumber = callData.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN ? callData.value.quote : callData.value.token;
-    recipientValue = toHex64BE(recipientValueNumber);
   }
 
+  const user_token_received_first_half_number: number = Math.floor(recipientValueNumber / 2);
+  const user_token_received_second_half_numner: number = recipientValueNumber - user_token_received_first_half_number;
+
+  if (user_token_received_first_half_number < 500) throw new Error("user_token_received_first_half_number is not enough");
+
+  const user_token_received_first_half = toHex64BE(user_token_received_first_half_number);
+  const user_token_received_second_half = toHex64BE(user_token_received_second_half_numner);
+
+  const poolNewLPValue = toHex64BE(Number(pool.lp.value));
+  const recipientScriptPubkey = getRecipientScriptPubkey(callData.recipientPublicKey);
+  const { txFee, serviceCommission } = getTxFeeServiceCommission(poolConfig.baseFee.number, poolConfig.serviceFee.number, callData.orderingFee); //  438, 1452   ???
   const tokenHolderCovenantScriptPubkey: string = poolConfig.holderCovenant.scriptpubkey.token;
   const lpHolderCovenantScriptPubkey: string = poolConfig.holderCovenant.scriptpubkey.lp;
   const mainHolderCovenantScriptPubkey: string = poolConfig.holderCovenant.scriptpubkey.main;
@@ -71,17 +73,24 @@ export const part2 = (pool: Pool, poolConfig: BmConfig, callData: CallData): str
     "01" +
     recipientAssetLE + // "RECEPIENT_ASSET_ID_REVERSE (L-BTC or TOKEN)" // 25d02aa3a6b673eefaaff069a84d32607f8756116b52520823bc3af84dbc3c21
     "01" +
-    recipientValue + // "RECEPIENT_VALUE" // 000000000eb8ebc0
+    user_token_received_first_half + // "RECEPIENT_VALUE" // 000000000eb8ebc0
     "00" +
     "16" +
     recipientScriptPubkey + // "RECEPIENT_SCRIPTPUBKEY" // 002062b5685478a2648d2d2eac4588fd5e8b51d9bdc34ebf942aa3310575a6227d52
     "01" +
-    qouteAssetLE + // "499a818545f6bae39fc03b637f2a4e1e64e590cac1bc3a6f6d71aa4443654c14" +
+    recipientAssetLE + // "RECEPIENT_ASSET_ID_REVERSE (L-BTC or TOKEN)" // 25d02aa3a6b673eefaaff069a84d32607f8756116b52520823bc3af84dbc3c21
     "01" +
-    "0000000000000000" +
+    user_token_received_second_half + // "RECEPIENT_VALUE" // 000000000eb8ebc0
     "00" +
-    "03" +
-    "6a01ff" +
+    "16" +
+    recipientScriptPubkey +
+    // "01" +
+    // qouteAssetLE + // "499a818545f6bae39fc03b637f2a4e1e64e590cac1bc3a6f6d71aa4443654c14" +
+    // "01" +
+    // "0000000000000000" +
+    // "00" +
+    // "03" +
+    // "6a01ff" +
     "01" +
     qouteAssetLE + // "499a818545f6bae39fc03b637f2a4e1e64e590cac1bc3a6f6d71aa4443654c14" +
     "01" +
