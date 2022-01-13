926464e3f342754069a1fb29636b819a571f85c1
Penguen Umut <uuisik@gmail.com>
2022-01-13 15:50:06 +0300

    catch: not enough ctx values

--- src/workers/createPoolTxWorker/method02.ts
+++ src/workers/createPoolTxWorker/method02.ts
@@ -6,26 +6,29 @@ import { sendRawTransaction } from "./helper/sendRawTransaction";
 
 export const method02 = async (pool: Pool, poolConfig: BmConfig, ctx: BmCtxNew): Promise<string | undefined> => {
   console.log("Pool tx creating on method 02 for ctx new id: " + ctx.commitmentTx.txid);
+  try {
+    if (pool.unspentTx === undefined) throw new Error("Unspent ptx yok!!! Nassi geldin buraya 2??");
 
-  if (pool.unspentTx === undefined) throw new Error("Unspent ptx yok!!! Nassi geldin buraya 2??");
+    const p1: string = part1(pool.unspentTx.txid, ctx.commitmentTx.txid);
+    // console.log("part1: " + p1);
 
-  const p1: string = part1(pool.unspentTx.txid, ctx.commitmentTx.txid);
-  // console.log("part1: " + p1);
+    const p2: string = part2(pool, poolConfig, ctx.callData);
+    // console.log("part2: " + p2);
 
-  const p2: string = part2(pool, poolConfig, ctx.callData);
-  // console.log("part2: " + p2);
+    const p3: string = await part3(pool, poolConfig, ctx);
+    // console.log("part3: " + p3);
 
-  const p3: string = await part3(pool, poolConfig, ctx);
-  // console.log("part3: " + p3);
+    const txhex = p1 + p2 + p3;
 
-  const txhex = p1 + p2 + p3;
+    const poolTxid = await sendRawTransaction(txhex);
 
-  const poolTxid = await sendRawTransaction(txhex);
-
-  if (poolTxid) {
-    console.log("poolTxid", poolTxid);
-    return poolTxid;
+    if (poolTxid) {
+      console.log("poolTxid", poolTxid);
+      return poolTxid;
+    }
+  } catch (error) {
+    throw new Error((error as any).message);
+  } finally {
+    return;
   }
-
-  return;
 };
