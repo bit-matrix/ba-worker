df21e0a5ca205ef0c153ed35d48727749b35dae3
Penguen Umut <uuisik@gmail.com>
2022-01-14 16:07:41 +0300

    unspentPoolTxInputsEncoded, unspentCtxsPoolTxInputsEncoded, poolTxInputsLengthHex

---
+++ src/workers/createPoolTxWorker/helper/poolTxInputs/poolTxInputs.ts
@@ -0,0 +1,17 @@
+import { unspentPoolTxInputs } from "./unspentPoolTxInputs";
+import { unspentCtxsPoolTxInputs } from "./ctxPoolTxInputs";
+
+export const poolTxInputs = (unspentPtxid: string, unspentCtxids: string[]): string => {
+  const poolTxInputsLength =
+    4 + // unspent pool tx outputs total lenght
+    2 * unspentCtxids.length; // unspent ctx outputs total lenght
+  const poolTxInputsLengthHex = poolTxInputsLength.toString(16).padStart(2, "0");
+
+  const unspentPoolTxInputsEncoded = unspentPoolTxInputs(unspentPtxid);
+
+  const unspentCtxsPoolTxInputsEncoded = unspentCtxsPoolTxInputs(unspentCtxids);
+
+  const poolTxInputsEncoded = "02000000" + "01" + poolTxInputsLengthHex + unspentPoolTxInputsEncoded + unspentCtxsPoolTxInputsEncoded;
+
+  return poolTxInputsEncoded;
+};
