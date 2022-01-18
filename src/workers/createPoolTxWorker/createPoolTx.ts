0123f9a3734855842e63b7d876572fa9c576cc72
Penguen Umut <uuisik@gmail.com>
2022-01-18 16:04:14 +0300

    createPoolTx added for all cases

---
+++ src/workers/createPoolTxWorker/createPoolTx.ts
@@ -0,0 +1,39 @@
+import { BmConfig, BmCtxNew, CallData, Pool } from "@bitmatrix/models";
+
+import { part1New } from "./helper/part1New";
+import { part2New } from "./helper/part2New";
+import { part3New } from "./helper/part3New";
+
+import { sendRawTransaction } from "./helper/sendRawTransaction";
+
+export const createPoolTx = async (pool: Pool, poolConfig: BmConfig, ctxs: BmCtxNew[]): Promise<string | undefined> => {
+  try {
+    if (pool.unspentTx === undefined) throw new Error("Unspent ptx yok!!! Nassi geldin buraya??");
+
+    const commitmentTxids: string[] = ctxs.map((c) => c.commitmentTx.txid);
+    const callDatas: CallData[] = ctxs.map((c) => c.callData);
+
+    const txInputsEncoded: string = part1New(pool.unspentTx.txid, commitmentTxids);
+    console.log("txInputsEncoded: " + txInputsEncoded);
+
+    const txOutputsEncoded: string = part2New(pool, poolConfig, callDatas);
+    console.log("txOutputsEncoded: " + txOutputsEncoded);
+
+    const txWitnessEncoded: string = await part3New(pool, poolConfig, ctxs);
+    console.log("txWitnessEncoded: " + txWitnessEncoded);
+
+    const txEncoded = txInputsEncoded + txOutputsEncoded + txWitnessEncoded;
+    console.log("txEncoded: " + txWitnessEncoded);
+
+    const poolTxid = await sendRawTransaction(txEncoded);
+
+    if (poolTxid) {
+      console.log("poolTxid", poolTxid);
+      return poolTxid;
+    }
+  } catch (error) {
+    throw new Error((error as any).message);
+  } finally {
+    return;
+  }
+};
