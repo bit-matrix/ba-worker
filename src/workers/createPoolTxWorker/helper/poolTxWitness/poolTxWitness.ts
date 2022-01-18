0d00c07715e0ca82590307312f836ceaeccdf3f4
Penguen Umut <uuisik@gmail.com>
2022-01-18 23:03:36 +0300

    poolTxWitness

--- src/workers/createPoolTxWorker/helper/poolTxWitness/poolTxWitness.ts
+++ src/workers/createPoolTxWorker/helper/poolTxWitness/poolTxWitness.ts
@@ -1,7 +1,31 @@
-import { BmCtxNew } from "@bitmatrix/models";
-import { getSettlements } from "./getSettlements";
+import { BmConfig, BmCtxNew, Pool } from "@bitmatrix/models";
+import { hexLE } from "@script-wiz/wiz-data";
+import { flagCovenant } from "./flagCovenant";
+import { lpHolderCovenant } from "./lpHolderCovenant";
+import { numberOfMainCovenantWitnessElements } from "./numberOfMainCovenantWitnessElements";
+import { settlements } from "./settlements";
+import { tokenHolderCovenant } from "./tokenHolderCovenant";
 
-export const poolTxWitness = async (ctx: BmCtxNew): Promise<string> => {
-  const settlements: string = await getSettlements(ctx.commitmentTx.txid);
-  return "" + settlements + "";
+export const poolTxWitness = async (pool: Pool, poolConfig: BmConfig, ctxs: BmCtxNew[]): Promise<string> => {
+  const poolAssetLE = hexLE(pool.id);
+
+  const flagCovenantEncoded = flagCovenant(poolConfig.holderCovenant.controlBlockPrefix.main);
+  const tokenHolderCovenantEncoded = tokenHolderCovenant(poolAssetLE, poolConfig.holderCovenant.controlBlockPrefix.token);
+  const lpHolderCovenantEncoded = lpHolderCovenant(poolAssetLE, poolConfig.holderCovenant.controlBlockPrefix.lp);
+
+  const numberOfMainCovenantWitnessElementsEncoded = numberOfMainCovenantWitnessElements(ctxs.length);
+
+  const settlementsEncoded: string = await settlements(ctxs);
+  const mainCovenantScript = poolConfig.mainCovenantScript;
+
+  return (
+    "0000" +
+    flagCovenantEncoded +
+    tokenHolderCovenantEncoded +
+    lpHolderCovenantEncoded +
+    settlementsEncoded +
+    numberOfMainCovenantWitnessElementsEncoded +
+    mainCovenantScript +
+    "TODO"
+  );
 };
