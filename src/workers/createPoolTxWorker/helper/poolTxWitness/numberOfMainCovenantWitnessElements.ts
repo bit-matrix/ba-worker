7c0bb1e2a7ba2b9693dad252920b296f15d7614f
Penguen Umut <uuisik@gmail.com>
2022-01-18 19:00:08 +0300

    numberOfMainCovenantWitnessElements

---
+++ src/workers/createPoolTxWorker/helper/poolTxWitness/numberOfMainCovenantWitnessElements.ts
@@ -0,0 +1,4 @@
+// Number of main covenant witness elements (23 = 2 + 7n [n = number of leafs]) // 17
+export const numberOfMainCovenantWitnessElements = (numberOfLeafs: number): string => {
+  return (2 + 7 * numberOfLeafs).toString(16).padEnd(2, "0");
+};
