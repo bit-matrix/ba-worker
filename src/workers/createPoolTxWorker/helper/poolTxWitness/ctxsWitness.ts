716612e68502e9a2738913d65bf8f0d219120735
Penguen Umut <uuisik@gmail.com>
2022-01-23 01:37:20 +0300

    ctxWitness double fix

--- src/workers/createPoolTxWorker/helper/poolTxWitness/ctxsWitness.ts
+++ src/workers/createPoolTxWorker/helper/poolTxWitness/ctxsWitness.ts
@@ -12,6 +12,14 @@ const ctxWitness = (ctx: BmCtxNew): string => {
     tapscript +
     "21" +
     tapscriptPrefix + // c4-c5
+    "1dae61a4a8f841952be3a511502d4f56e889ffa0685aa0098773ea2d4309f624" +
+    "00000003" + // commitment input 2 witness
+    "0101" +
+    // tapscript lenght
+    "5c" +
+    tapscript +
+    "21" +
+    tapscriptPrefix + // c4-c5
     "1dae61a4a8f841952be3a511502d4f56e889ffa0685aa0098773ea2d4309f624"
   );
 };
