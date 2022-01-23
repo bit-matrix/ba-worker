2fb48fb4884f5a03303b9bab7c42cec5ea8f3994
Penguen Umut <uuisik@gmail.com>
2022-01-23 19:31:11 +0300

    poolTxTxFeeOutputEncoded fix

--- src/workers/createPoolTxWorker/helper/poolTxOutputs/poolTxTxFeeOutput.ts
+++ src/workers/createPoolTxWorker/helper/poolTxOutputs/poolTxTxFeeOutput.ts
@@ -4,8 +4,7 @@ import { hexLE } from "@script-wiz/wiz-data";
 export const poolTxTxFeeOutput = (qouteAsset: string, txFeeValueHex: string): string => {
   const qouteAssetLE = hexLE(qouteAsset);
 
-  const poolTxTxFeeOutputEncoded = "01" + qouteAssetLE + "01" + txFeeValueHex;
-  "00" + "00" + "00000000";
+  const poolTxTxFeeOutputEncoded = "01" + qouteAssetLE + "01" + txFeeValueHex + "00" + "00" + "00000000";
 
   return poolTxTxFeeOutputEncoded;
 };
