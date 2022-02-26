1feec7b13865538e4fd489c15931aad0f75df217
PenguenUmut <uuisik@gmail.com>
2022-02-26 16:11:17 +0300

    createPoolTx new result types

--- src/workers/createPoolTxWorker/createPoolTx.ts
+++ src/workers/createPoolTxWorker/createPoolTx.ts
@@ -1,4 +1,4 @@
-import { BmConfig, BmCtxNew, CallData, Pool } from "@bitmatrix/models";
+import { BmConfig, BmCtxNew, BmNewPtxResult, CallData, Pool } from "@bitmatrix/models";
 
 import { part1New } from "./helper/part1New";
 import { part2New } from "./helper/part2New";
@@ -6,9 +6,11 @@ import { part3New } from "./helper/part3New";
 
 import { sendRawTransaction } from "./helper/sendRawTransaction";
 
-export const createPoolTx = async (pool: Pool, poolConfig: BmConfig, ctxs: BmCtxNew[]): Promise<string | undefined> => {
+export const createPoolTx = async (pool: Pool, poolConfig: BmConfig, ctxs: BmCtxNew[]): Promise<BmNewPtxResult> => {
   try {
     if (pool.unspentTx === undefined) throw new Error("Unspent ptx yok!!! Nassi geldin buraya??");
+
+    const result: BmNewPtxResult = { poolTx: undefined, ctxsResult: [] };
 
     const commitmentTxids: string[] = ctxs.map((c) => c.commitmentTx.txid);
     const callDatas: CallData[] = ctxs.map((c) => c.callData);
@@ -16,7 +18,7 @@ export const createPoolTx = async (pool: Pool, poolConfig: BmConfig, ctxs: BmCtx
     const txInputsEncoded: string = part1New(pool.unspentTx.txid, commitmentTxids);
     console.log("txInputsEncoded: " + txInputsEncoded);
 
-    const txOutputsEncoded: string = part2New(pool, poolConfig, callDatas);
+    const { txOutputsEncoded, isOutOfSlippages } = part2New(pool, poolConfig, callDatas);
     console.log("txOutputsEncoded: " + txOutputsEncoded);
 
     const txWitnessEncoded: string = await part3New(pool, poolConfig, ctxs);
@@ -29,8 +31,11 @@ export const createPoolTx = async (pool: Pool, poolConfig: BmConfig, ctxs: BmCtx
 
     if (poolTxid) {
       console.log("poolTxid", poolTxid);
-      return poolTxid;
+      result.poolTx = poolTxid;
+      result.ctxsResult = ctxs.map((c, i) => ({ bmCtxNew: c, isOutOfSlippage: isOutOfSlippages[i], error: undefined }));
     }
+
+    return result;
   } catch (error) {
     throw new Error((error as any).message);
   }
