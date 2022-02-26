11deea55baf6d2d4945ae8eb7ccad60cfc4fe7ef
PenguenUmut <uuisik@gmail.com>
2022-02-26 16:11:34 +0300

    ba-db new ctx mempool changes

--- src/workers/createPoolTxWorker/index.ts
+++ src/workers/createPoolTxWorker/index.ts
@@ -1,10 +1,10 @@
 import { Block } from "@bitmatrix/esplora-api-client";
-import { BmConfig, BmCtxNew, Pool } from "@bitmatrix/models";
+import { BmConfig, BmCtxNew, BmNewPtxResult, Pool } from "@bitmatrix/models";
 import { config, ctxMempoolSave } from "../../business/db-client";
 import { topCtxs } from "./helper/common";
 import { createPoolTx } from "./createPoolTx";
 
-export const createPoolTxWorker = async (pool: Pool, newBlock: Block, newCtxs: BmCtxNew[]): Promise<string | undefined> => {
+export const createPoolTxWorker = async (pool: Pool, newBlock: Block, newCtxs: BmCtxNew[]): Promise<BmNewPtxResult | undefined> => {
   if (newCtxs.length === 0) return;
   console.log("Create ptx worker for newCtxs started for pool: " + pool.id + ". newBlockheight: " + newBlock.height + ", newCtxs.count: " + newCtxs.length);
 
@@ -13,15 +13,21 @@ export const createPoolTxWorker = async (pool: Pool, newBlock: Block, newCtxs: B
   const bestCtxs: BmCtxNew[] = topCtxs(newCtxs, poolConfig.maxLeaf);
   console.log(bestCtxs.length + " bestCtxs for pool tx: ", bestCtxs.map((c) => c.commitmentTx.txid).join(","));
 
-  const ptxid: string | undefined = await createPoolTx(pool, poolConfig, bestCtxs);
+  const bmNewPtxResult: BmNewPtxResult = await createPoolTx(pool, poolConfig, bestCtxs);
 
-  if (ptxid) {
+  if (bmNewPtxResult.poolTx) {
     for (let i = 0; i < bestCtxs.length; i++) {
       const ctxNew = bestCtxs[i];
 
-      await ctxMempoolSave(pool.id, { callData: ctxNew.callData, output: ctxNew.output, commitmentTx: ctxNew.commitmentTx, poolTxid: ptxid });
+      await ctxMempoolSave(pool.id, {
+        callData: ctxNew.callData,
+        output: ctxNew.output,
+        commitmentTx: ctxNew.commitmentTx,
+        poolTxid: bmNewPtxResult.poolTx,
+        isOutOfSlippage: bmNewPtxResult.ctxsResult[i].isOutOfSlippage,
+      });
     }
   }
 
-  return ptxid;
+  return bmNewPtxResult;
 };
