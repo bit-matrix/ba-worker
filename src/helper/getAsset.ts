767cb03347a185f23df20ef53f23ee27e0a1997d
afarukcali <afarukcali@gmail.com>
2022-05-26 16:24:06 +0300

    get asset promise function wait error fixed.

--- src/helper/getAsset.ts
+++ src/helper/getAsset.ts
@@ -1,7 +1,7 @@
 import { esploraClient, UserIssuedAsset } from "@bitmatrix/esplora-api-client";
 
 export const getAsset = async (assetId: string): Promise<UserIssuedAsset | undefined> => {
-  const asset = esploraClient.asset(assetId) as any as UserIssuedAsset;
+  const asset = (await esploraClient.asset(assetId)) as any as UserIssuedAsset;
   if (asset.chain_stats === undefined) return undefined;
   return asset;
 };
