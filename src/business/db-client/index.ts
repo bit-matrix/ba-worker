977d98e4008767252c968bdc94cb768148ae894e
afarukcali <afarukcali@gmail.com>
2022-05-31 17:37:06 +0300

    config add request added.

--- src/business/db-client/index.ts
+++ src/business/db-client/index.ts
@@ -12,6 +12,8 @@ export const poolUpdate = (newPool: Pool): Promise<void> => axios.post<void>(DB_
 
 export const config = (asset: string): Promise<BmConfig> => axios.get<BmConfig>(DB_URL + "config/" + asset).then((res) => res.data);
 
+export const configAdd = (newConfig: BmConfig): Promise<void> => axios.post<void>(DB_URL + "config", newConfig).then((res) => res.data);
+
 export const ctxsNew = (asset: string): Promise<BmCtxNew[]> =>
   axios
     .get<BmCtxNew[]>(DB_URL + "ctx/" + asset)
