379cf6f46c305fdb24157f1fa843ecf8894ba591
afarukcali <afarukcali@gmail.com>
2022-06-04 14:19:54 +0300

    App fetch block unsynced and sycned states review.

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -5,14 +5,15 @@ import { poolWorker } from "./poolWorker";
 import * as nodeCron from "node-cron";
 import { AppSync } from "../appSync";
 
-const worker = async (newBlockHash: string, bestBlockHash: string) => {
+// bitmatrix tx seperation
+const ctxptxworker = async (newBlockHash: string, bestBlockHash: string) => {
   try {
     console.log("pool tx , commitment tx worker started..");
 
     const ps = await pools();
     const promises: Promise<void>[] = [];
     const newBlock = await esploraClient.block(newBlockHash);
-    const bestBlock = await esploraClient.block(newBlockHash);
+    const bestBlock = await esploraClient.block(bestBlockHash);
 
     for (let i = 0; i < ps.length; i++) {
       const p = ps[i];
@@ -29,27 +30,27 @@ const worker = async (newBlockHash: string, bestBlockHash: string) => {
 };
 
 const getFinalBlockDetail = async () => {
-  const bestBlockHeight = await esploraClient.blockTipHeight();
-  const bestBlockHash = await esploraClient.blockheight(bestBlockHeight);
-
-  console.log("bestBlockHeight", bestBlockHeight);
-
   const appLastState = await getLastAppSyncState("1");
 
-  console.log("appLastState", appLastState.blockHeight);
+  // app synced state
+  if (appLastState.synced) {
+    const bestBlockHeight = await esploraClient.blockTipHeight();
+    const bestBlockHash = await esploraClient.blockheight(bestBlockHeight);
 
-  if (!appLastState.synced) {
-    appWorker();
-  }
+    // new block found
+    if (bestBlockHeight - appLastState.blockHeight === 1) {
+      await poolRegisteryWorker(bestBlockHeight, bestBlockHash);
+      await ctxptxworker(appLastState.blockHash, bestBlockHash);
 
-  if (appLastState.synced && bestBlockHeight - appLastState.blockHeight === 1) {
-    await poolRegisteryWorker(bestBlockHeight, bestBlockHash);
-    await worker(appLastState.blockHash, bestBlockHash);
+      const newDbState: AppSync = { id: "1", blockHash: bestBlockHash, blockHeight: bestBlockHeight, synced: true };
+      await updateAppSyncState(newDbState);
 
-    const newDbState: AppSync = { id: "1", blockHash: bestBlockHash, blockHeight: bestBlockHeight, synced: true };
-    await updateAppSyncState(newDbState);
-  } else if (appLastState.synced && bestBlockHeight - appLastState.blockHeight > 1) {
-    appWorker();
+      console.log("sync completed");
+    }
+    // synced app restarted
+    else if (bestBlockHeight - appLastState.blockHeight > 1) {
+      appWorker();
+    }
   }
 };
 
@@ -57,30 +58,32 @@ const appWorker = async () => {
   console.log("app syncer started..");
 
   try {
-    const bestBlockHeight = await esploraClient.blockTipHeight();
-    const bestBlockHash = await esploraClient.blockheight(bestBlockHeight);
-
     const appLastState = await getLastAppSyncState("1");
 
-    if (bestBlockHeight > appLastState.blockHeight) {
-      console.log("case1", "bestblock : ", bestBlockHeight, "lastblock : ", appLastState.blockHeight);
+    // app unsycned state
+    if (!appLastState.synced) {
+      const bestBlockHeight = await esploraClient.blockTipHeight();
+      const bestBlockHash = await esploraClient.blockheight(bestBlockHeight);
 
-      const nextBlockHeight = appLastState.blockHeight + 1;
+      if (bestBlockHeight > appLastState.blockHeight) {
+        const nextBlockHeight = appLastState.blockHeight + 1;
 
-      const nextBlockHash = await esploraClient.blockheight(nextBlockHeight);
+        const nextBlockHash = await esploraClient.blockheight(nextBlockHeight);
 
-      await poolRegisteryWorker(nextBlockHeight, nextBlockHash);
-      await worker(appLastState.blockHash, bestBlockHash);
+        await poolRegisteryWorker(nextBlockHeight, nextBlockHash);
+        await ctxptxworker(appLastState.blockHash, bestBlockHash);
 
-      const newDbState: AppSync = { id: "1", blockHash: nextBlockHash, blockHeight: nextBlockHeight, synced: false };
+        const newDbState: AppSync = { id: "1", blockHash: nextBlockHash, blockHeight: nextBlockHeight, synced: false };
 
-      await updateAppSyncState(newDbState);
+        await updateAppSyncState(newDbState);
 
-      appWorker();
-    } else if (bestBlockHeight === appLastState.blockHeight) {
-      const newDbState: AppSync = { ...appLastState, synced: true };
+        appWorker();
+      } else if (bestBlockHeight === appLastState.blockHeight) {
+        console.log("block çektim eşitledim update ediyorum statei");
+        const newDbState: AppSync = { ...appLastState, synced: true };
 
-      await updateAppSyncState(newDbState);
+        await updateAppSyncState(newDbState);
+      }
     }
   } catch (error) {
     console.error("appWorker.error", error);
@@ -94,4 +97,5 @@ nodeCron.schedule("*/10 * * * * *", () => {
 export const startWorkers = async () => {
   console.log("startWorkers started...");
   init("https://electrs.basebitmatrix.com/");
+  appWorker();
 };
