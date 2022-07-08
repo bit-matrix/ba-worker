d2e191063e4a98748f52b50af84b8f02f79d7cbf
reyhanboydag <reyhanboydag@gmail.com>
2022-07-08 17:26:25 +0300

    pool tx worker work started

--- src/workers/v2/poolTxWorker/index.ts
+++ src/workers/v2/poolTxWorker/index.ts
@@ -1,26 +1,42 @@
+import { TxDetail } from "@bitmatrix/esplora-api-client";
 import { TxDetailRPC } from "@bitmatrix/models";
 import { pools } from "../../../business/db-client";
 import { RedisClient } from "../../../redisClient/RedisClient";
-import { poolTxFinder } from "./poolTxFinder";
+import { findNewPtxWorker } from "../../findNewPtxWorker";
+import { validateAndBroadcastPoolTx } from "./validateAndBroadcastPoolTx";
 
 const redisClient = new RedisClient("redis://localhost:6379");
 
-export const poolTxWorker = async () => {
+export const poolTxWorker = async (txDetails: TxDetail[]) => {
   //redisten ctx'leri cekme.
   //todo: valide etme
-  const values = await redisClient.getAllValues();
+  const values: any = await redisClient.getAllValues();
 
   //TODO:Pool transaction Id'leri update etme
 
   //Pool validasyonlarından geçirme
-  console.log(values, "values");
-  const ps = await pools();
-  for (let i = 0; i < values.length; i++) {
-    const newTxDetail = values[i] as TxDetailRPC;
-    await poolTxFinder(newTxDetail.txid, ps);
+  if (values.length > 0) {
+    const waitingCommitmentList: any[] = values.filter((value: any) => !value.txId);
+    const waitingPoolTxList: any[] = values.filter((value: any) => value.txId);
 
-    // if (newTxDetail.field !== true)
-    await redisClient.updateField(newTxDetail.txid, true);
-    console.log("here");
+    if (waitingCommitmentList.length > 0) {
+      const ps = await pools();
+
+      if (ps.length > 0) {
+        for (let i = 0; i < waitingCommitmentList.length; i++) {
+          const commitmentTx = waitingCommitmentList[i];
+          const poolTxId = await validateAndBroadcastPoolTx(commitmentTx, ps);
+
+          await redisClient.updateField(commitmentTx.txId, poolTxId.txId);
+        }
+      }
+    }
+
+    if (waitingPoolTxList.length > 0) {
+      for (let i = 0; i < waitingPoolTxList.length; i++) {
+        const waitingPoolTx = waitingPoolTxList[i];
+        //const poolTxId = await findNewPtxWorker(txDetails);
+      }
+    }
   }
 };
