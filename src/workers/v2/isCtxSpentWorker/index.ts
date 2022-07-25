b29dc3a93bec89af6ed5e189753f3548d892e38c
afarukcali <afarukcali@gmail.com>
2022-07-25 19:59:06 +0300

    isCtxSpentWorker simple design.

--- src/workers/v2/isCtxSpentWorker/index.ts
+++ src/workers/v2/isCtxSpentWorker/index.ts
@@ -1,7 +1,20 @@
+import { esploraClient } from "@bitmatrix/esplora-api-client";
 import { CTXFinderResult } from "@bitmatrix/models";
 import { redisClient } from "../../../redisClient/redisInit";
 
 export const isCtxSpentWorker = async () => {
   console.log("-------------------IS CTX SPENT WORKER-------------------------");
   const waitingTxs = await redisClient.getAllValues<CTXFinderResult>();
+
+  if (waitingTxs.length > 0) {
+    for (const tx of waitingTxs) {
+      const outspends = await esploraClient.txOutspends(tx.transaction.txid);
+
+      if (outspends[1].spent || outspends[2].spent) {
+        await redisClient.removeKey(tx.transaction.txid);
+
+        // add tx history
+      }
+    }
+  }
 };
