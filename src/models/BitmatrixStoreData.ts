a18ded94b26673a7907e6758af532800b9a921b1
afarukcali <afarukcali@gmail.com>
2022-07-27 17:17:25 +0300

    BitmatrixStoreData updated.

--- src/models/BitmatrixStoreData.ts
+++ src/models/BitmatrixStoreData.ts
@@ -2,6 +2,5 @@ import { CTXFinderResult, PTXFinderResult } from "@bitmatrix/models";
 
 export type BitmatrixStoreData = {
   commitmentData: CTXFinderResult;
-  poolValidationData?: PTXFinderResult;
   poolTxId?: string;
 };
