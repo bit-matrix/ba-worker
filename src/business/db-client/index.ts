ff7ad3555c229071aee392c82e030268eccf620b
afarukcali <afarukcali@gmail.com>
2022-07-27 15:02:48 +0300

    ptx save function revert.

--- src/business/db-client/index.ts
+++ src/business/db-client/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { DB_URL } from "../../env";
-import { BmConfig, Pool, AppSync } from "@bitmatrix/models";
+import { BmConfig, Pool, AppSync, BmPtx } from "@bitmatrix/models";
 
 export const pools = (): Promise<Pool[]> => axios.get<Pool[]>(DB_URL + "pools").then((res) => res.data);
 export const pool = (asset: string): Promise<Pool> => axios.get<Pool>(DB_URL + "pools/" + asset).then((res) => res.data);
@@ -13,3 +13,13 @@ export const config = (): Promise<BmConfig> => axios.get<BmConfig>("https://raw.
 // appSync
 export const getLastAppSyncState = (): Promise<AppSync> => axios.get<AppSync>(DB_URL + "appSync").then((res) => res.data);
 export const updateAppSyncState = (newState: AppSync): Promise<void> => axios.post<void>(DB_URL + "appSync", newState).then((res) => res.data);
+
+// temp
+export const ptxSave = (asset: string, value: BmPtx): Promise<void> =>
+  axios
+    .post(DB_URL + "ptx/" + asset, value)
+    .then((res) => res.data)
+    .catch((res) => {
+      console.error("ctxMempoolSave", res.message);
+      throw res.message;
+    });
