bdb4d587f00ff3f47da775f45359fed9cfd2d7cd
afarukcali <afarukcali@gmail.com>
2022-08-01 23:09:50 +0300

    worker delay removed.

--- src/env.local.ts
+++ src/env.local.ts
@@ -1,4 +1,3 @@
 // local
 export const ELECTRS_URL = process.env.DB_URL || "https://electrs.basebitmatrix.com/";
 export const DB_URL = process.env.DB_URL || "https://rocksdb.basebitmatrix.com/";
-export const WORKER_DELAY = Number(process.env.DB_URL || 5);
--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,3 @@
 // local
 export const ELECTRS_URL = process.env.DB_URL || "http://127.0.0.1:3000/";
 export const DB_URL = process.env.DB_URL || "http://127.0.0.1:4499/";
-export const WORKER_DELAY = Number(process.env.DB_URL || 5);
