e9852c2dbc461652a77f8493371aecd99b6bf3d9
afarukcali <afarukcali@gmail.com>
2022-08-08 23:49:32 +0300

    history db url updated.

--- src/env.ts
+++ src/env.ts
@@ -2,4 +2,4 @@
 export const ELECTRS_URL = process.env.ELECTRS_URL || "http://127.0.0.1:3000/";
 export const DB_URL = process.env.DB_URL || "http://127.0.0.1:4450/";
 export const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
-export const HISTORY_DB_URL = process.env.HISTORY_DB_URL || "http://127.0.0.1:9901/";
+export const HISTORY_DB_URL = process.env.HISTORY_DB_URL || "http://127.0.0.1:9902/";
