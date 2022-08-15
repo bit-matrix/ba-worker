c441d63e10a66f855c0167899def2ac2eb6efa27
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-08-15 15:28:53 +0300

    sendSlackMessage trying with user token

--- src/helper/sendSlackMessage.ts
+++ src/helper/sendSlackMessage.ts
@@ -1,6 +1,6 @@
 import { LogLevel, WebClient } from "@slack/web-api";
 
-const BOT_USER_OAUTH_TOKEN = "xoxp-1891434848919-2754300617426-3920888349718-5033902fc0a147a62011ed7bea556597";
+const BOT_USER_OAUTH_TOKEN = "xoxp-1891434848919-2754300617426-3964185336672-22b0495ed5ed01c9cb014ab5475429b3";
 const channelId = "C03T3JHGMMX";
 
 export const sendSlackMessage = async (message: string) => {
@@ -19,7 +19,7 @@ export const sendSlackMessage = async (message: string) => {
   //   console.error("sendSlackMessage.error", error);
   // }
 
-  const client = new WebClient("xoxb-your-token", {
+  const client = new WebClient("xoxp-1891434848919-2754300617426-3964185336672-22b0495ed5ed01c9cb014ab5475429b3", {
     // LogLevel can be imported and used to make debugging simpler
     logLevel: LogLevel.DEBUG,
   });
@@ -29,7 +29,7 @@ export const sendSlackMessage = async (message: string) => {
     // Call the chat.postMessage method using the built-in WebClient
     const result = await client.chat.postMessage({
       // The token you used to initialize your app
-      token: "xoxp-1891434848919-2754300617426-3920888349718-5033902fc0a147a62011ed7bea556597",
+      token: "xoxp-1891434848919-2754300617426-3964185336672-22b0495ed5ed01c9cb014ab5475429b3",
       channel: "C03T3JHGMMX",
       text: message,
       // You could also use a blocks[] array to send richer content
