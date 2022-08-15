aca710d772458449d494f6ab250b965c0de944ca
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-08-15 14:36:53 +0300

    sendSlackMessage: changing token

--- src/helper/sendSlackMessage.ts
+++ src/helper/sendSlackMessage.ts
@@ -1,21 +1,43 @@
 import { LogLevel, WebClient } from "@slack/web-api";
 
-const BOT_USER_OAUTH_TOKEN = "xoxb-1891434848919-3894455745527-cNshA5dN0UH2yAgVPI2WKBel";
+const BOT_USER_OAUTH_TOKEN = "xoxp-1891434848919-2754300617426-3920888349718-5033902fc0a147a62011ed7bea556597";
 const channelId = "C03T3JHGMMX";
 
-export const sendSlackMessage = (message: string) => {
+export const sendSlackMessage = async (message: string) => {
   console.log("Message", message + "\n ------------- ");
 
-  const client = new WebClient(BOT_USER_OAUTH_TOKEN, {
+  // const client = new WebClient(BOT_USER_OAUTH_TOKEN, {
+  //   logLevel: LogLevel.DEBUG,
+  // });
+
+  // try {
+  //   const result = client.chat.postMessage({
+  //     channel: channelId,
+  //     text: message,
+  //   });
+  // } catch (error) {
+  //   console.error("sendSlackMessage.error", error);
+  // }
+
+  const client = new WebClient("xoxb-your-token", {
+    // LogLevel can be imported and used to make debugging simpler
     logLevel: LogLevel.DEBUG,
   });
 
+  // Post a message to a channel your app is in using ID and message text
   try {
-    const result = client.chat.postMessage({
-      channel: channelId,
+    // Call the chat.postMessage method using the built-in WebClient
+    const result = await client.chat.postMessage({
+      // The token you used to initialize your app
+      token: "xoxp-1891434848919-2754300617426-3920888349718-5033902fc0a147a62011ed7bea556597",
+      channel: "C03T3JHGMMX",
       text: message,
+      // You could also use a blocks[] array to send richer content
     });
+
+    // Print result, which includes information about the message (like TS)
+    console.log(result);
   } catch (error) {
-    console.error("sendSlackMessage.error", error);
+    console.error(error);
   }
 };
