05ad4dfe8a98c471fbebc065730fe740ab84a12e
reyhanboydag <reyhanboydag@gmail.com>
2022-09-02 00:52:36 +0300

    unused files deleted

--- src/env.local.ts
+++
@@ -1,4 +0,0 @@
-// local
-export const ELECTRS_URL = process.env.DB_URL || "https://electrs.basebitmatrix.com/";
-export const DB_URL = process.env.DB_URL || "https://rocksdb.basebitmatrix.com/";
-export const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
