806e718a248f0b7aaf0afabd7e0c6c9c2ed325ab
afarukcali <afarukcali@gmail.com>
2022-09-11 14:26:59 +0300

    price calc fixed.

--- src/helper/tokenPriceCalculation.ts
+++ src/helper/tokenPriceCalculation.ts
@@ -2,9 +2,13 @@ import { PAsset, Pool } from "@bitmatrix/models";
 import { lbtcAsset } from "./util";
 
 export const tokenPriceCalculation = (token: PAsset, quote: PAsset): number => {
-  if (quote.assetHash === lbtcAsset) {
-    return Number(token.value) / Number(quote.value);
+  if (quote.value && quote.value) {
+    if (quote.assetHash === lbtcAsset) {
+      return Number(token.value) / Number(quote.value);
+    }
+
+    return ((Number(quote.value) * Math.pow(10, 8 - quote.precision)) / Number(token.value)) * Math.pow(10, 8 - token.precision);
   }
 
-  return Number(quote.value) / Number(token.value);
+  return 0;
 };
