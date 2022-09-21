fc3e8c8c604aff499f1b5422755b32e5c6dbdc9c
afarukcali <afarukcali@gmail.com>
2022-09-21 16:49:49 +0300

    unused api calls removed.

--- src/business/db-client/index.ts
+++ src/business/db-client/index.ts
@@ -1,14 +1,12 @@
 import axios from "axios";
 import { DB_URL, HISTORY_DB_URL } from "../../env";
-import { BmConfig, Pool, AppSync, BmPtx, BmChart, CommitmentTxHistory } from "@bitmatrix/models";
+import { Pool, AppSync, BmChart, CommitmentTxHistory } from "@bitmatrix/models";
 
 export const pools = (): Promise<Pool[]> => axios.get<Pool[]>(DB_URL + "pools").then((res) => res.data);
 export const pool = (asset: string): Promise<Pool> => axios.get<Pool>(DB_URL + "pools/" + asset).then((res) => res.data);
 
 // TODO
 export const poolUpdate = (newPool: Pool): Promise<void> => axios.post<void>(DB_URL + "pools", newPool).then((res) => res.data);
-
-export const config = (): Promise<BmConfig> => axios.get<BmConfig>("https://raw.githubusercontent.com/bit-matrix/bitmatrix-app-config/master/testnet.json").then((res) => res.data);
 
 // appSync
 export const getLastAppSyncState = (): Promise<AppSync> => axios.get<AppSync>(DB_URL + "appSync").then((res) => res.data);
